d1c987a13cc3ab139c1ba8c953ca16930361447d
Jessica-Garcia <jessica.rodriguesgarcia@gmail.com>
2023-03-08 15:46:28 -0300

    refactor: configure global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { styled } from "@/styles"
 
 const Button = styled('button', {
 
-  backgroundColor: '$primary',
+  backgroundColor: '$green300',
   borderRadios: 4,
   border: 0,
   padding: '4px 8px',
