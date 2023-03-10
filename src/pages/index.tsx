68c3b54b6428c2e72a0aa9f6ecf317d8edf82221
Jessica-Garcia <jessica.rodriguesgarcia@gmail.com>
2023-03-10 10:37:08 -0300

    refactor: fix layout errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
-import { styled } from "@/styles";
-import { HomeConatiner, Product } from "@/styles/pages/home";
+import { HomeContainer, Product } from "@/styles/pages/home";
 import Image from "next/image";
 import tShirt1 from '../assets/t-shirts/1.png'
 import tShirt2 from '../assets/t-shirts/2.png'
@@ -7,14 +6,16 @@ import tShirt3 from '../assets/t-shirts/3.png'
 
 export default function Home() {
   return (
-    <HomeConatiner>
+    <HomeContainer>
       <Product>
         <Image src={tShirt1} width={520} height={480} alt=''/>
+        
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
+      
       <Product>
         <Image src={tShirt2} width={520} height={480} alt=''/>
         <footer>
@@ -22,6 +23,6 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
-    </HomeConatiner>
+    </HomeContainer>
   )
 }
