f72d22142430366daf366217a7b1809614328e2c
Jessica-Garcia <jessica.rodriguesgarcia@gmail.com>
2023-03-17 11:54:40 -0300

    feat: stripe checkout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,10 +12,16 @@ interface ProductProps {
         imageUrl: string;
         price: string;
         description: string;
+        defaultPriceId: string;
     }
 }
 
 export default function Product( {product}: ProductProps ) {
+
+    const handleBuyProduct = () => {
+        console.log(product.defaultPriceId)
+    }
+
     const {isFallback} = useRouter();
 
     if(isFallback){
@@ -32,7 +38,7 @@ export default function Product( {product}: ProductProps ) {
                 <h1> {product.name} </h1>
                 <span>{product.price}</span>
                 <p>{product.description}</p>
-                <button>
+                <button onClick={handleBuyProduct}>
                     Comprar agora
                 </button>
             </ProductDatails>
@@ -77,6 +83,7 @@ export const getStaticProps: GetStaticProps<any, {id: string }> = async({ params
               currency: "BRL",
             }).format(price.unit_amount! / 100,),
             description: product.description,
+            defaultPriceId: price.id,
         }
       },
       revalidate: 60 * 60 * 1,
