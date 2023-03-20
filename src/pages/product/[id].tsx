c3f16f06473baed79d9acc17260b2d8490bf5a59
Jessica-Garcia <jessica.rodriguesgarcia@gmail.com>
2023-03-20 11:51:34 -0300

    feat: redirect to checkout page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,10 @@
 import { stripe } from "@/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDatails } from "@/styles/pages/product";
+import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import Stripe from "stripe";
 
 interface ProductProps {
@@ -18,8 +20,24 @@ interface ProductProps {
 
 export default function Product( {product}: ProductProps ) {
 
-    const handleBuyProduct = () => {
-        console.log(product.defaultPriceId)
+    const [isCreatingCheckoutSession, setIsCreatingCheckoutSession ] = useState(false)
+
+    const handleBuyProduct = async () => {
+        try {
+            setIsCreatingCheckoutSession(true)
+            const response = await axios.post('/api/checkout', {
+                priceId: product.defaultPriceId,
+            })
+            
+            const {checkoutUrl} = response.data
+
+            window.location.href = checkoutUrl
+
+        } catch (error) {
+            setIsCreatingCheckoutSession(false)
+
+            alert('falha ao redirecionar ao checkout!')
+        }
     }
 
     const {isFallback} = useRouter();
@@ -38,14 +56,13 @@ export default function Product( {product}: ProductProps ) {
                 <h1> {product.name} </h1>
                 <span>{product.price}</span>
                 <p>{product.description}</p>
-                <button onClick={handleBuyProduct}>
+                <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
                     Comprar agora
                 </button>
             </ProductDatails>
         </ProductContainer>
     )
 }
- 
 
 export const getStaticPaths: GetStaticPaths = async() => {
      
